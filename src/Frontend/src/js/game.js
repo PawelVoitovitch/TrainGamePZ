8c71254fd2aa032c6185bd22d11177449adc450e
Jakub Zając <jakub.zajac2611@gmail.com>
2023-04-05 10:58:48 +0200

    PZ-71

--- src/Frontend/src/js/game.js
+++ src/Frontend/src/js/game.js
@@ -2,23 +2,48 @@ const urlParams = new URLSearchParams(window.location.search);
 const gameId = urlParams.get("gameId");
 const url = "http://localhost:8090/game";
 const container = document.querySelector("body");
-async function getPlayers(gameId) {
-	try {
-		const response = await fetch(`${url}/${gameId}/players`);
-		if (!response.ok) {
-			throw new Error("Network response was not ok");
-		}
-		const players = await response.json();
-		players.forEach((player) => {
-			const newPlayerName = document.createElement("p");
-			newPlayerName.classList.add("playerName");
-			newPlayerName.innerHTML = `<p class="playerName">${player.login}</p>`;
-			container.appendChild(newPlayerName);
-		});
-		return players;
-	} catch (error) {
-		console.error("Error:", error);
-	}
-}
+const matchDestination = document.querySelector(".matchDestination");
+const destinationPopup = document.querySelector(".destinationPopup");
+const closeBtn = document.querySelector(".closeBtn");
+const routeBox = document.getElementById("one");
+const tableName = document.querySelector(".tableName-name");
 
-getPlayers(gameId);
+// async function getPlayers(gameId) {
+// 	try {
+// 		const response = await fetch(`${url}/${gameId}/players`);
+// 		if (!response.ok) {
+// 			throw new Error("Network response was not ok");
+// 		}
+// 		const players = await response.json();
+// 		players.forEach((player) => {
+// 			const newPlayerName = document.createElement("p");
+// 			newPlayerName.classList.add("playerName");
+// 			newPlayerName.innerHTML = `<p class="playerName">${player.login}</p>`;
+// 			container.appendChild(newPlayerName);
+// 		});
+// 		return players;
+// 	} catch (error) {
+// 		console.error("Error:", error);
+// 	}
+// }
+
+tableName.textContent = `Game ID: ${gameId}`;
+
+matchDestination.addEventListener("click", () => {
+	destinationPopup.style.opacity = 1;
+	destinationPopup.style.zIndex = 100;
+});
+
+closeBtn.addEventListener("click", () => {
+	destinationPopup.style.opacity = 0;
+	destinationPopup.style.zIndex = -100;
+});
+
+// getPlayers(gameId);
+
+routeBox.addEventListener("click", () => {
+	let children = routeBox.querySelectorAll("*");
+	for (let i = 0; i < children.length; i++) {
+		children[i].style.backgroundColor = "red";
+	}
+});
